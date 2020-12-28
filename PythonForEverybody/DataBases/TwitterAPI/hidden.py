8151195e1bb187098f5b50be59b7a95619cb32b8
Tudor Mihai-Eugen <tudormihaieugen@gmail.com>
2020-12-28 13:49:48 +0200

    Added db and ws

---
+++ PythonForEverybody/DataBases/TwitterAPI/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "MjwsQ7PZHy97I9Ny6isTdXvU9",
+            "consumer_secret": "SsvzEX7NmhzRXW9YMw2UtH1Tdsg9N1TMy5B9Z8xFxgOPMJ5shh",
+            "token_key": "911235786904887296-UoBl1yJKwOFzZRjMHkEcmvtFrQG0XRU",
+            "token_secret": "yQBdxhoLoXf8JL8P9xfQZrbMi5SA7ya14G4PApaTkyWOo"}
